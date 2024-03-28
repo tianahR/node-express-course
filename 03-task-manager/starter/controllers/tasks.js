8a72491e0e6ea5afd4cbb4e8764f0c54130845f5
tianahR <farahrabenjam@yahoo.com>
2024-03-27 22:11:21 -0400

    Connecting to Mongo DB, Externalizing Secrets in a .env File Using the Mongoose NPM Package Mongo Models for Schema

---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,35 @@
+const Task = require('../models/tasks') //schema
+
+const getAllTasks = (req,res)=>{
+    
+    res.send('get all Tasks')
+}
+
+const createTask = async (req, res) => {
+    const task = await Task.create(req.body)
+    res.status(201).json({ task })
+  }
+
+const getTask = (req,res)=>{
+    
+    // res.send('get single task')
+    res.json({id:req.params.id})
+}
+
+const updateTask = (req,res)=>{
+    
+    res.send('update task')
+}
+
+const deleteTask = (req,res)=>{
+    
+    res.send('delete tasks')
+}
+
+module.exports =  {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+}
