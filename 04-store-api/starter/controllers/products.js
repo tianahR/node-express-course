ff39ef164db3325f5b38d6284e2702f72cfb6228
tianahR <farahrabenjam@yahoo.com>
2024-04-06 16:57:19 -0400

    Use of Mongoose skip and select filter for pagination funtionalality

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -4,7 +4,11 @@ const getAllProductsStatic = async (req,res)=>{
 
     // throw new Error('Testing async errors')
     // const products = await Product.find({featured: true} )
-    const products = await Product.find({}).sort('-name price')
+    const products = await Product.find({})
+    .sort('name')
+    .select('name price')
+    .limit(10) //use skip and limit for pagination functionality
+    .skip(5)
     res.status(200).json({products, nbHits: products.length})
 }
 
@@ -16,7 +20,7 @@ const getAllProducts = async (req,res)=>{
 
     //to make sure that the properties to use in the query exists
 
-    const { featured, company, name, sort } = req.query;
+    const { featured, company, name, sort, fields } = req.query;
     const queryObject = {};
 
     if (featured) {
@@ -43,13 +47,35 @@ const getAllProducts = async (req,res)=>{
     // sort
     let result = Product.find(queryObject);
 
+    // use mongoose sort filter
     if(sort){
         const sortList = sort.split(',').join(' ');
         result = result.sort(sortList);
     }else {
         result = result.sort('createdAt');
     }
-    
+
+    //end of use mongoose sort filter
+
+    // use of mongoose select filter 
+
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ');
+        result = result.select(fieldsList);
+    }
+
+    //end of use of mongoose select filter
+
+    // use of mongoose skip and limit filter for pagination functionality
+
+    const page = Number(req.query.page) || 1; //default 1
+    const limit = Number(req.query.limit) || 10; //default 10
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+
+    //end of pagination
+
     const products = await result;
 
     res.status(200).json({products, nbHits: products.length})
