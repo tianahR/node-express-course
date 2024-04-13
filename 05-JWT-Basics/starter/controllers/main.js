4e2a5b8448c55972ee886154e8459870f445dc62
tianahR <farahrabenjam@yahoo.com>
2024-04-13 12:35:09 -0400

    Postman setup, check in the controller if username and password exist

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,4 +1,21 @@
+
+// if exist create new JWT
+// send back to frontend
+// setup authentication so only the request with JWT can access the dasboard
+
+
+const CustomAPIError = require('../errors/custom-error')
+
 const login = async(req,res)=>{
+
+    const { username, password } = req.body //// check username, password in post(login) request
+    //console.log(username,password)
+
+    //check if username and password exist - if not throw error
+    if (!username || !password) {
+        throw new CustomAPIError('Please provide email and password',400)
+      }
+
     res.send('Fake Login/Register/SignUp Route')
 }
 
